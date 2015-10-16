590f1facedb5b509f562c4e3fe8a82a0784cf2f3
Makoto Nishijima <nishijima.web.craft@gmail.com>
2015-10-16 23:10:50 +0900

    タイトル出力ヘルパーの作成
    静的ページのHomeページのタイトル出力の修正
    テストの修正

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+  def full_title(page_title)
+    base_title = "Railsチュートリアル サンプルアプリ"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
