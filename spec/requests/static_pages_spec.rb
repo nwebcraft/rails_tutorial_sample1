fc06bc3f3c88c2ce60c380e402e8779a41f0a305
Makoto Nishijima <nishijima.web.craft@gmail.com>
2015-10-17 13:15:31 +0900

    ５章演習
    full_titleヘルパーのテスト
    テスト重複部分のDRY化

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,36 +2,62 @@ require 'spec_helper'
 
 describe "静的ページ" do
 
-  let(:base_title) { "Railsチュートリアル サンプルアプリ" }
+  #let(:base_title) { "Railsチュートリアル サンプルアプリ" }
 
   subject { page }
 
+  shared_examples_for "静的ページの表示について" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Homeページ" do
     before { visit root_path }
+    let(:heading)    { 'Home' }
+    let(:page_title) { '' }
 
-    it { should have_content('Home') }
-    it { should have_title("#{base_title}") }
+    it_should_behave_like "静的ページの表示について"
     it { should_not have_title(" | Home") }
   end
 
   describe "Helpページ" do
     before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help") }
+    it_should_behave_like "静的ページの表示について"
   end
 
   describe "Aboutページ" do
     before { visit about_path }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' }
 
-    it { should have_content('About Us') }
-    it { should have_title("#{base_title} | About Us") }
+    it_should_behave_like "静的ページの表示について"
   end
 
   describe "Contactページ" do
     before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it_should_behave_like "静的ページの表示について"
+  end
+
+  it "should have right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_content('About Us')
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_content('Help')
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_content('Contact')
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_content('Sign up')
+    expect(page).to have_title(full_title('Sign Up'))
   end
 end
